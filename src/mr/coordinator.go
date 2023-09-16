99a400facf752a70f78e6b2ed6cd0bcaddb2f52c
LYH <3210101621@zju.edu.cn>
2023-09-16 13:56:39 +0800

    finished

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,16 +1,38 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
+var mu sync.Mutex
+type CoordinatorPhase int
+const (
+	MapPhase CoordinatorPhase = iota
+	ReducePhase
+	ExitPhase
+)
+type TaskInfo struct {
+	TaskReference *Task
+	startTime time.Time
+	isFinished bool
+	isExcuting bool
+}
 
 type Coordinator struct {
 	// Your definitions here.
-
+	InputFiles    []string
+	nReduce int
+	intermediateFilenamesForPerReduce [][]string // [reduceTaskNumber][intermediateFilenames]。第一维度长度是nReduce，第二维度是这个reduceTaskNumber对应的所有intermediateFilenames
+	CoordinatorPhase CoordinatorPhase
+	Taskqueue chan *Task
+	TaskInfos map[int]*TaskInfo
 }
+
 
 // Your code here -- RPC handlers for the worker to call.
 
@@ -23,8 +45,65 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
-
-
+func (c *Coordinator) TaskExample(args *ExampleArgs, reply *Task) error {
+	mu.Lock()
+	defer mu.Unlock()
+	reply.TaskType = mapTask
+	reply.TaskNumber = 1
+	reply.FilenameForMap = "pg-*.txt"
+	reply.NReduce = 2
+	reply.IntermediateFilenames = []string{"mr-0-0", "mr-0-1"}
+	reply.IntermediateFilenames = []string{"mr-1-0", "mr-1-1"}
+	return nil
+}
+func (c *Coordinator) GetTask(args *ExampleArgs, reply *Task) error {
+	mu.Lock()
+	defer mu.Unlock()
+	if(len(c.Taskqueue) > 0) {
+		*reply = *<-c.Taskqueue
+		c.TaskInfos[reply.TaskNumber].startTime = time.Now()
+		c.TaskInfos[reply.TaskNumber].isExcuting = true
+	} else if(c.CoordinatorPhase != ExitPhase){
+		reply.TaskType = wait
+	} else {
+		reply.TaskType = exit
+	}
+	log.Println("Coordinator Get(put) task: ", reply)
+	return nil
+}
+func (c *Coordinator) FinishTask(args *Task, reply *ExampleReply) error {
+	log.Println("Coordinator FinishTask first line:Finish task ing: ", args)
+	mu.Lock()
+	defer mu.Unlock()
+	log.Println("Coordinator Finish task ing: ", args)
+	if(args.TaskType != c.TaskInfos[args.TaskNumber].TaskReference.TaskType || c.TaskInfos[args.TaskNumber].isFinished) {
+		return nil
+	}
+	c.TaskInfos[args.TaskNumber].isFinished = true
+	c.TaskInfos[args.TaskNumber].isExcuting = false
+	if args.TaskType == mapTask {
+		for i, filename := range args.IntermediateFilenames {
+			c.intermediateFilenamesForPerReduce[i] = append(c.intermediateFilenamesForPerReduce[i], filename)
+		}
+		if(c.allTasksFinished()) {
+			c.CoordinatorPhase = ReducePhase
+			c.createReduceTasks()
+		}
+	}else if args.TaskType == reduceTask {
+		if(c.allTasksFinished()) {
+			c.CoordinatorPhase = ExitPhase
+		}
+	}
+	return nil
+}
+func (c *Coordinator) allTasksFinished() bool {
+	for _, taskInfo := range c.TaskInfos {
+		if !taskInfo.isFinished {
+			return false
+		}
+	}
+	return true
+}
 //
 // start a thread that listens for RPCs from worker.go
 //
@@ -46,12 +125,9 @@ func (c *Coordinator) server() {
 // if the entire job has finished.
 //
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
+	mu.Lock()
+	defer mu.Unlock()
+	return c.CoordinatorPhase == ExitPhase
 }
 
 //
@@ -60,11 +136,82 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
-
-	// Your code here.
-
-
+	c := Coordinator{
+		InputFiles: files,
+		nReduce: nReduce,
+		intermediateFilenamesForPerReduce: make([][]string, nReduce),
+		CoordinatorPhase: MapPhase,
+		Taskqueue: make(chan *Task, max(len(files), nReduce)),
+		TaskInfos: make(map[int]*TaskInfo),
+	}
+	c.createMapTasks()
 	c.server()
+	go c.catchTaskTimeout()
 	return &c
+}
+func (c *Coordinator) createMapTasks() {
+	for i, filename := range c.InputFiles {
+		task :=Task{
+			TaskType: mapTask,
+			TaskNumber: i,
+			FilenameForMap: filename,
+			NReduce: c.nReduce,
+			IntermediateFilenames: []string{},
+		}
+		c.Taskqueue <- &task
+		c.TaskInfos[i] = &TaskInfo{
+			TaskReference: &task,
+			isFinished: false,
+			isExcuting: false,
+		}
+	}
+}
+func (c *Coordinator) createReduceTasks() {
+	log.Println("createReduceTasks ing")
+	for i := 0; i < c.nReduce; i++ {
+		log.Println("createReduceTasking:i=", i)
+		task :=Task{
+			TaskType: reduceTask,
+			TaskNumber: i,
+			NReduce: c.nReduce,
+			IntermediateFilenames: c.intermediateFilenamesForPerReduce[i],
+		}
+		c.Taskqueue <- &task
+		log.Println("createReduceTasking:task=", task)
+		c.TaskInfos[i] = &TaskInfo{
+			TaskReference: &task,
+			isFinished: false,
+		}
+	}
+}
+
+func (c *Coordinator) catchTaskTimeout() {
+	for {
+		time.Sleep(4 * time.Second)
+		log.Println("catchTaskTimeout ing,mu.Lock() ing")
+		mu.Lock()
+		log.Println("catchTaskTimeout ing,mu.Lock() excuted")
+		if c.CoordinatorPhase == ExitPhase {
+			log.Println("catchTaskTimeout ing,mu.Unlock() ing")
+			mu.Unlock()
+			log.Println("catchTaskTimeout ing,mu.Unlock() excuted")
+			return
+		}
+		for _, taskInfo := range c.TaskInfos {
+			if taskInfo.isExcuting && !taskInfo.isFinished && time.Since(taskInfo.startTime) > 20 * time.Second {
+				c.Taskqueue <- taskInfo.TaskReference
+				taskInfo.startTime = time.Now()
+			}
+		}
+		log.Println("catchTaskTimeout ing,mu.Unlock() ing")
+		mu.Unlock()
+		log.Println("catchTaskTimeout ing,mu.Unlock() excuted")
+	}
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,17 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-
+import (
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+	"time"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -13,8 +20,30 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+// for sorting by key.
+type ByKey []KeyValue
 
-//
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+type taskType int
+const (
+	mapTask =0
+	reduceTask =1
+	wait =2
+	exit =3
+)
+type Task struct {
+	FilenameForMap string
+	TaskType taskType
+	NReduce int
+	TaskNumber int
+	IntermediateFilenames []string
+	ReducerOutputFilename string
+}
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
 //
@@ -30,12 +59,108 @@ func ihash(key string) int {
 //
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
-
-	// Your worker implementation here.
-
 	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
+	//CallExample()
+	//CalltaskExample()
+	// Your worker implementation here.
+	for{
+		task := CallgetTask()
+		switch task.TaskType {
+		case mapTask:
+			mapper(&task, mapf)
+		case reduceTask:
+			reducer(&task, reducef)
+		case wait:
+			time.Sleep(5 * time.Second)
+		case exit:
+			return
+		}
+	}
+}
 
+func mapper(task *Task, mapf func(string, string) []KeyValue) {
+	log.Println("worker in mapper ing...")
+	content, err := ioutil.ReadFile(task.FilenameForMap)
+	if err != nil {
+		log.Fatal("Failed to read file: "+task.FilenameForMap, err)
+		//todo , call coordinator to report error
+	}
+	kva := mapf(task.FilenameForMap, string(content))
+	for i := 0; i < task.NReduce; i++ {
+		intermediate := []KeyValue{}
+		for _, kv := range kva {
+			if ihash(kv.Key) % task.NReduce == i {
+				intermediate = append(intermediate, kv)
+			}
+		}
+		task.IntermediateFilenames = append(task.IntermediateFilenames, WriteIntermediate(task.TaskNumber, i, intermediate))
+	}
+	CallFinishTask(task)
+}
+
+
+func reducer(task *Task, reducef func(string, []string) string) {
+	intermediate := *ReadIntermediate(task.IntermediateFilenames)
+	sort.Sort(ByKey(intermediate))
+	dir, _ := os.Getwd()
+	outputFileFullName := filepath.Join(dir, fmt.Sprintf("mr-out-%d", task.TaskNumber))
+	outputFile, err := os.Create(outputFileFullName)
+	if err != nil {
+		log.Fatal("Failed to create file: "+outputFileFullName, err)
+	}
+	for i := 0; i < len(intermediate); {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+		fmt.Fprintf(outputFile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	outputFile.Close()
+	task.ReducerOutputFilename = outputFileFullName
+	CallFinishTask(task)
+}
+
+func WriteIntermediate(taskNumber int, reduceNumber int, intermediate []KeyValue) string {
+	filename := fmt.Sprintf("mr-%d-%d", taskNumber, reduceNumber)
+	dir,_ := os.Getwd()
+	file, err := ioutil.TempFile(dir, filename)
+	if err != nil {
+		log.Fatal("Failed to create temp file: "+filename, err)
+	}
+	for _, kv := range intermediate {
+		fmt.Fprintf(file, "%v %v\n", kv.Key, kv.Value)
+	}
+	file.Close()
+	return file.Name()
+}
+func ReadIntermediate(filenames []string) *[]KeyValue {
+	kva := []KeyValue{}
+	for _, filename := range filenames {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatal("Failed to open file: "+filename, err)
+		}
+		content, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatal("Failed to read file: "+filename, err)
+		}
+		file.Close()
+		for _, line := range strings.Split(string(content), "\n") {
+			if line == "" {
+				continue
+			}
+			kv := KeyValue{}
+			fmt.Sscanf(line, "%s %s", &kv.Key, &kv.Value)
+			kva = append(kva, kv)
+		}
+	}
+	return &kva
 }
 
 //
@@ -66,7 +191,37 @@ func CallExample() {
 		fmt.Printf("call failed!\n")
 	}
 }
+func CalltaskExample() Task {
+	args := ExampleArgs{}
+	reply := Task{}
+	ok := call("Coordinator.TaskExample", &args , &reply)
+	log.Println("worker TaskExample: ", reply)
+	if !ok {
+		reply.TaskType = wait
+	}
+	return reply
+}
 
+func CallgetTask() Task {
+	args := ExampleArgs{}
+	reply := Task{}
+	ok := call("Coordinator.GetTask", &args , &reply)
+	log.Println("worker Get task: ", reply)
+	if !ok {
+		reply.TaskType = wait
+	}
+	return reply
+}
+func CallFinishTask(task *Task) {
+	log.Println("worker finish task ing...")
+	args := task
+	reply := ExampleReply{}
+	ok := call("Coordinator.FinishTask", &args , &reply)
+	log.Println("worker finish task:",task,reply)
+	if !ok {
+		log.Fatal("Failed to finish task: ", task)
+	}
+}
 //
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
@@ -83,9 +238,10 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 
 	err = c.Call(rpcname, args, reply)
 	if err == nil {
+		log.Println("worker call: ", rpcname, args, reply)
 		return true
 	}
-
+	
 	fmt.Println(err)
 	return false
 }
